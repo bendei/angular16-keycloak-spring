2f65217b5441601d6e2f2ee3e20031becb7a71bc
Istvan Bende <istvan.bende@rise-world.com>
2023-11-03 08:57:29 +0100

    playing with async/await

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, isDevMode, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { environment } from '../environments/environment';
 
+import { mymodulenevem, myfunction } from './myjsmoduleone';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,17 +14,39 @@ export class AppComponent implements OnInit {
 
   apiUrl = environment.apiUrl;
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<Ember | null> {
     console.log("apiUrl: " + this.apiUrl);
 
     if (isDevMode()) {
       console.log("DEV MODE");
+
+      console.log("working with module: " + mymodulenevem +  " " + myfunction(2,3));
+
+      myfunk().then(result => {console.log(result)});
+
+
+      const prom = Promise.resolve(new Ember("en pistike", 51) );
+      const resu = await prom;
+      const {neve} = resu;
+      console.log(neve);
+      return resu;
     } else {
       console.log("PROD MODE");
+      return null;
     }
   }
+}
 
+async function myfunk() {
+  return 1;
+}
 
+class Ember {
+  neve: string;
+  kora: number
 
-
+  constructor(n: string, k: number) {
+    this.neve = n;
+    this.kora = k;
+  }
 }
---
+++ frontend/src/app/myjsmoduleone.ts
@@ -0,0 +1,4 @@
+
+export const mymodulenevem = 'psiti vagyok';
+
+export const myfunction = (a, b) => {return a + b};
