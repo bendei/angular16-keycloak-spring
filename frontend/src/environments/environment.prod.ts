09e5fd08476afee7e7297b64fd252d170178ed0f
bende <istvan_bende@yahoo.com>
2024-05-20 18:10:39 +0200

    frontend: openAPI generated files moved under core folder

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ import {DefaultService} from "../app/openapi-generated-sources";
 
 export const environment = {
   production: true,
-  //apiUrl: 'http://localhost:8081/api',
-  apiUrl: 'http://192.168.1.65:',
+  apiUrl: 'http://192.168.1.65:8081/api',
+  //apiUrl: 'http://192.168.1.65:',
   apiService: DefaultService,
   keycloak: {
     issuer: 'http://localhost:8080',
